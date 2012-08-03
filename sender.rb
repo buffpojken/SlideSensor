f209f2086e7394f26ace6dbe96e63c036cc6b5cb
buffpojken <daniel@sykewarrior.com>
2012-08-03 13:51:05 +0200

    lots of tweaks

--- sender.rb
+++ sender.rb
@@ -1,10 +1,41 @@
 require 'socket'                                         
 require 'json'
 
+# payload = {"type"=>"state", "data"=>{"node"=>{"started"=>"2012-08-02 20:01:53", "pid"=>22467, 
+#   "child"=>22468, "device"=>{"error"=>0, "majorVersion"=>2, "minorVersion"=>9, "buildVersion"=>930, 
+#     "name"=>"BX PLC Server\u0000\u0000"}, "state"=>{"error"=>0, "adsState"=>5, "deviceState"=>0}}, 
+#    "Interface"=>{
+#      "ibPump"=>false, "ibRelease"=>false, "ibForceRed"=>false, "ibForceYellow"=>false, "
+#      ibForceGreen"=>false, "obPumping"=>false, "obReady"=>true, "obRed"=>false, 
+#      "obYellow"=>false, "obGreen"=>false, "obFalseStart"=>false, "onTime1"=>0, 
+#      "onTime2"=>0, "onTime3"=>0, "onTimeTotal"=>0, "onState"=>0, 'temperature' => 16.4}}, "from"=>"beckhoff"}
+# 
+# sock = UDPSocket.new
+# 
+# payload['data']['Interface']['obPumping'] = true
+# payload['data']['Interface']['onTime1'] = Time.now.to_i
+# sock.send(payload.to_json, 0, '0.0.0.0', 8282)
+# sleep 5
+# payload['data']['Interface']['onTime2'] = Time.now.to_i
+# sock.send(payload.to_json, 0, '0.0.0.0', 8282)
+# sleep 5
+# payload['data']['Interface']['onTime3'] = Time.now.to_i
+# payload['data']['Interface']['obYellow'] = true
+# sock.send(payload.to_json, 0, '0.0.0.0', 8282)   
+# sleep 5
+# payload['data']['Interface']['obPumping'] = true
+# payload['data']['Interface']['onTimeTotal'] = Time.now.to_i
+# sock.send(payload.to_json, 0, '0.0.0.0', 8282)
+# 
+# sock.close
 
-
-sock = UDPSocket.new
-data = {:key => "temperature", :value => "laser"}.to_json
-sock.send(data, 0, '0.0.0.0', 8080)
+sock = UDPSocket.new                  
+payload = {
+  "to"    => "beckhoff", 
+  "cmd"   => "set", 
+  "tag"   => "ibPump",
+  "from"  => "slider",
+  "value" => true
+}
+sock.send(payload.to_json+"\n", 0, 'kanan.vassaro.net', 8282)
 sock.close
-
--- slide_demon.rb
+++ slide_demon.rb
@@ -1,7 +1,106 @@
 require 'eventmachine'
 require 'mysqlplus'
 require 'em-mysqlplus'
-require 'json'
+require 'evma_httpserver'
+require 'json'  
+require 'cgi'
+
+class APIServer < EM::Connection
+  include EM::HttpServer
+
+  def post_init
+    super
+    no_environment_strings
+  end        
+
+  def process_http_request
+    params = CGI::parse(@http_query_string)    
+    puts params.inspect   
+    if @http_path_info == "/pump"
+      $pump.toggle
+    end
+    if @http_path_info == "/light"
+      $light.force(params["light"][0])
+    end
+    response = EM::DelegatedHttpResponse.new(self)
+    response.status = 200
+    response.content_type 'text/javascript'     
+    status = {:pump => $pump.state}
+    response.content = "#{params["callback"][0]}(#{status.to_json})"
+    response.send_response      
+  end
+end
+
+class Pump 
+  attr_accessor :state
+  def set(data)
+    self.state = data['obPumping'] 
+  end  
+  
+  def toggle
+    payload = {
+      "to"    => "beckhoff", 
+      "cmd"   => "set", 
+      "tag"   => "ibPump",
+      "from"  => "slider",
+      "value" => !self.state
+    }          
+    self.state = !self.state
+    $oc.send(payload.to_json+"\n", 0)
+  end
+end
+
+class Trafficlight
+  attr_accessor :state, :captured
+
+  def initialize
+    self.state = nil
+    self.captured = false
+  end    
+  
+  def force(light)   
+    payload = {
+      "to"    => "beckhoff", 
+      "cmd"   => "set", 
+      "from"  => "slider",
+    }        
+    case light
+      when "green"
+        payload['tag'] = "ibForceGreen"
+        payload['value'] = true
+      when "yellow"
+        payload['tag'] = "ibForceYellow"
+        payload['value'] = true
+       when "red"
+        payload['tag'] = "ibForceRed"
+        payload['value'] = true
+      end   
+          
+     puts payload.inspect
+#    $oc.send(payload.to_json+"\n", 0)    
+  end
+
+  def set(data)
+    self.red = data['obRed']
+    self.green = data['obGreen']
+    self.yellow = data['obYellow']
+  end           
+
+  private
+
+  def red=(flag)
+    self.state = :red if flag
+  end
+
+  def yellow=(flag)
+    self.state = :yellow if flag
+  end
+
+  def green=(flag)
+    self.state = :green if flag
+  end         
+
+end
 
 class Ride
   attr_accessor :points, :start, :stop, :state
@@ -11,7 +110,9 @@ class Ride
   end
 
   def <<(data)
-    self.start = data["onTime1"]
+    self.start = data["onTime1"]      
+    self.points[0] = data['onTime2']
+    self.points[1] = data['onTime3']
     self.stop  = data["onTimeTotal"]
     self.state = data['onState']
     if data['onTimeTotal'] != 0
@@ -20,8 +121,14 @@ class Ride
   end                                 
 
   def save
-    puts "Saving..."
-  end
+    q = $db.query("insert into photos(temperature, ride_time, ride_no, timestamp_1, timestamp_2, timestamp_3, created_at, updated_at) select #{$temperature}, #{self.stop}, count(*)+1, #{self.start}, #{self.points[0]}, #{self.points[1]}, NOW(), NOW() from photos")    
+    q.callback do |res|
+      $ride = Ride.new
+    end
+    q.errback do |res|
+      puts res.inspect      
+    end
+  end  
 end                                   
 
 
@@ -36,24 +143,56 @@ end
 #             "onTime3":0,"onTimeTotal":0,"onState":0}},
 # "from":"beckhoff"}         
 
-module SensorParser  
+class SensorParser < EventMachine::Connection
   def receive_data(data) 
     data = JSON.parse(data)
     puts data.inspect
     unless data["type"] == "state"
       return
-    end
-    $ride << data['data']["Interface"]   
-    puts $ride.inspect
-  end  
+    end                    
+
+    $light.set(data['data']['Interface'])           
+    $pump.set(data['data']['Interface'])
+    manage_temperature(data['data']['Interface']['temperature'])
+
+    $ride << data['data']["Interface"]                   
+
+    puts $light.inspect
+    puts $ride.inspect       
+    puts $pump.inspect
+  end   
+
+  private 
+
+  def manage_temperature(temp)
+    unless $temperature == temp  
+      q = $db.query("insert into data_points(data_key, data_value, created_at, updated_at) values('temperature', '"+temp.to_s+"', NOW(), NOW())")
+      q.callback do |res|
+        $temperature = temp
+      end                                                      
+      q.errback do |res|                                                 
+        puts res
+      end
+    end    
+  end 
+
 end  
 
 
 cl = UDPSocket.new
 cl.setsockopt(Socket::SOL_SOCKET,Socket::SO_REUSEADDR,1)
 cl.bind('0.0.0.0', 8282)
+                           
+$oc = UDPSocket.new                  
+$oc.connect('kanan.vassaro.net', 8282)
 
-EventMachine::run {  
-	$ride = Ride.new
-	read = EventMachine.attach(cl, SensorParser)
+EventMachine::run {         
+  $db = EventMachine::MySQL.new(:host => "localhost", :username => "root", :database => "kanan_dev")  
+  $ride           = Ride.new        
+  $light          = Trafficlight.new
+  $temperature    = 0                
+  $pump           = Pump.new                
+  
+  EM.start_server '0.0.0.0', 4568, APIServer
+  read = EventMachine.attach(cl, SensorParser)
 }
